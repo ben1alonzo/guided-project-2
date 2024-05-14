ca23a4f72050d92cd1409b562d8728f0c1fd0a03
Ben Alonzo <balonzo@travelers.com>
2024-05-14 12:05:04 -0400

    partway through part 3, adding server.js functionality

--- server/server.js
+++ server/server.js
@@ -23,12 +23,110 @@ app.get("/planets", async (req, res) => {
   try {
     const client = await MongoClient.connect(url);
     const db = client.db(dbName);
-    const collection = db.collection(planetsCollection);
-    const planets = await collection.find({}).toArray();
-    res.json(planets);
+    const collection = db.collection(filmsCollection);
+    const films = await collection.find({}).toArray();
+    res.json(films);
   } catch (err) {
     console.error("Error:", err);
     res.status(500).send("Hmmm, something smells...");
+  }
+});
+
+app.get("/characters", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(charactersCollection);
+    const characters = await collection.find({}).toArray();
+    res.json(characters);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Hmmm, something smells...");
+  }
+});
+
+app.get("/films", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(filmsCollection);
+    const films = await collection.find({}).toArray();
+    res.json(films);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Hmmm, something smells...");
+  }
+});
+
+const getElementById = async (elementId, aCollection) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(aCollection);
+    const elements = await collection
+      .find({ id: parseInt(elementId) })
+      .toArray();
+    return elements;
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Internal server error");
+  }
+};
+
+app.get("/films/:id", async (req, res) => {
+  try {
+    const film_id = req.params.id;
+    const films = await getElementById(film_id, filmsCollection);
+    res.json(films);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/characters:id", async (req, res) => {
+  try {
+    const characters_id = req.params.id;
+    const characters = await getElementById(
+      characters_id,
+      charactersCollection
+    );
+    res.json(characters);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/planets:id", async (req, res) => {
+  try {
+    const planets_id = req.params.id;
+    const planets = await getElementById(planets_id, planetsCollection);
+    res.json(planets);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/films/:id/characters", async (req, res) => {
+  try {
+    const film_id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(filmsCharactersCollection);
+    const film_character_keys = await collection
+      .find({ film_id: film_id })
+      .toArray();
+    const characters = await Promise.all(
+      film_character_keys.map(async (id) => {
+        return await getElementById(id.character_id, charactersCollection);
+      })
+    );
+    res.json(characters);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
